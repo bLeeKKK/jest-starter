d6db61152f70b36798c04c51349043ebd9f12aa9
lb <1961774471@qq.com>
2022-06-22 16:31:10 +0800

    1.初始化jeat，初步学习

---
+++ tests/utils/storage.test.ts
@@ -0,0 +1,8 @@
+import storage from "@/utils/storage";
+
+describe('storage', () => {
+  it('缓存值', () => {
+    storage.set('newKey', "hello");
+    expect(localStorage.getItem("my-app-newKey")).toEqual('hello');
+  })
+});
