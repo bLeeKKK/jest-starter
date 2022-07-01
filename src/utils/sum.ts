0035bf2a44b11c15ea23f3bf82b65add3aae3b5c
lb <1961774471@qq.com>
2022-07-01 16:27:28 +0800

    1.联系测试函数

--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -1,3 +1,47 @@
-const sum = (a: number, b: number) => a + b;
+export const sum = (a: number, b: number) => a + b;
+export const subtraction = (a: number, b: number) => a - b;
 
-export default sum;
+export const lengthOfLongestSubstring = (str: string): number => {
+  let maxCount = 0
+  let maxArr: string[] = []
+  const arr = str.split('')
+  arr
+    .forEach((item) => {
+      const index = maxArr.findIndex(res => item === res)
+      if (index === -1) {
+        maxArr.push(item);
+        if (maxCount < maxArr.length) maxCount = maxArr.length;
+      } else {
+        if (maxCount < maxArr.length) maxCount = maxArr.length;
+        maxArr.splice(0, index + 1);
+        maxArr.push(item);
+      }
+    })
+  return maxCount
+}
+
+export const checkPossibility = (nums: number[]): boolean => {
+  const len = nums.length
+  if (len < 3 || isPossibSort(nums)) {
+    return true
+  }
+  for (let i = 0; i < len; i++) {
+    if (i + 1 < len && nums[i] > nums[i + 1]) {
+      nums[i] = nums[i + 1];
+      return isPossibSort(nums);
+    }
+  }
+  return false
+}
+
+export const isPossibSort = (nums: number[]): boolean => {
+  const len = nums.length
+  for (let i = 0; i < len; i++) {
+    if (i + 1 < len) {
+      if (nums[i] > nums[i + 1]) {
+        return false
+      }
+    }
+  }
+  return true
+}
--- tests/utils/sum.test.ts
+++ tests/utils/sum.test.ts
@@ -1,7 +1,47 @@
-import sum from 'utils/sum'
+import {
+  sum,
+  subtraction,
+  lengthOfLongestSubstring,
+  // checkPossibility,
+  isPossibSort
+} from 'utils/sum';
 
 describe('sum', () => {
   it('做加法', () => {
     expect(sum(1, 1)).toEqual(2)
   })
 })
+
+describe("subtraction", () => {
+  it("做减法", () => {
+    expect(subtraction(2, 1)).toEqual(1)
+  })
+})
+
+describe("lengthOfLongestSubstring", () => {
+  it("最长子串", () => {
+    expect(lengthOfLongestSubstring("abcabcbb")).toEqual(3)
+    expect(lengthOfLongestSubstring("bbbbb")).toEqual(1)
+    expect(lengthOfLongestSubstring("pwwkew")).toEqual(3)
+  })
+})
+
+describe("isPossibSort", () => {
+  it(" 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列", () => {
+    expect(isPossibSort([4, 2, 3])).toEqual(false);
+    expect(isPossibSort([4, 2, 1])).toEqual(false);
+    expect(isPossibSort([3, 4, 2, 3])).toEqual(false);
+    expect(isPossibSort([5, 7, 1, 8])).toEqual(false);
+    expect(isPossibSort([1, 2, 3, 4])).toEqual(true);
+    expect(isPossibSort([1, 2, 2, 4])).toEqual(true);
+  })
+})
+
+// describe("checkPossibility", () => {
+//   it(" 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列", () => {
+//     expect(checkPossibility([4, 2, 3])).toEqual(true);
+//     expect(checkPossibility([4, 2, 1])).toEqual(false);
+//     expect(checkPossibility([3, 4, 2, 3])).toEqual(false);
+//     expect(checkPossibility([5, 7, 1, 8])).toEqual(true);
+//   })
+// })
