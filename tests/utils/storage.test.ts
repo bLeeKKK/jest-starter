3fef2bb938f053ca36833a55a8ab13909da0577f
lb <1961774471@qq.com>
2022-07-01 16:57:45 +0800

    1.测试文件

--- tests/utils/storage.test.ts
+++ tests/utils/storage.test.ts
@@ -1,8 +1,16 @@
 import storage from "utils/storage";
 
 describe('storage', () => {
-  it('缓存值', () => {
+  it('设置缓存值', () => {
     storage.set('newKey', "hello");
     expect(localStorage.getItem("my-app-newKey")).toEqual('hello');
   })
+
+  it('获取缓存值', () => {
+    localStorage.setItem("my-app-newKey", "value");
+    expect(storage.get('newKey')).toEqual('value');
+
+    // localStorage.setItem("my-app-newKey", "hello");
+    // expect(storage.get("newKey")).toEqual("hello");
+})
 });
